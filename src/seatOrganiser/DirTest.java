f391d812ace92c24e6af867548afc23c38561b5b
calvinkinzie <kinzie.calvin@gmail.com>
2015-05-20 03:32:45 +0000

    Update DirTest.java

--- src/seatOrganiser/DirTest.java
+++ src/seatOrganiser/DirTest.java
@@ -1,8 +1,8 @@
 package seatOrganiser;
 
+/**Checks user directory, and returns the path to said directory.*/
 public class DirTest {
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
 	}
-
 }
